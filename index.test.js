1fef9975e14da5fcc92de7a1fd06469fddff169a
enisdenjo <badurinadenis@gmail.com>
2023-05-09 18:51:04 +0200

    configurable iterations

--- index.test.js
+++ index.test.js
@@ -1,6 +1,5 @@
 describe("should not leak", () => {
-  const iterations = 300;
-  for (let i = 0; i < iterations; i++) {
+  for (let i = 0; i < process.env.ITERATIONS; i++) {
     test("does it?", async () => {
       await fetch("http://localhost:58080");
     });
